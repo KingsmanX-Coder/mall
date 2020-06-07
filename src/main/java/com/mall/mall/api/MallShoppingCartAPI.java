6a3af482bfc33c42ba90c1d0ab0958030b3ddabf
KingsmanX-Coder <63635213+KingsmanX-Coder@users.noreply.github.com>
2020-06-08 01:06:01 +0800

    1、购物车 2、用户地址

---
+++ src/main/java/com/mall/mall/api/MallShoppingCartAPI.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (C), 2020-2020
+ * FileName: MallShoppingCartAPI
+ * Author:   Kingsman
+ * Date:     2020/6/8 0:52
+ * Description:
+ */
+
+package com.mall.mall.api;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Api(value = "v1", tags = "5.购物车相关接口")
+@RequestMapping("/api/v1")
+public class MallShoppingCartAPI {
+
+    @Resource
+private MallShoppingCartService MallShoppingCartService;
+
+    @GetMapping("/shop-cart/page")
+    @ApiOperation(value = "购物车列表(每页默认5条)", notes = "传参为页码")
+    public Result<PageResult<List<MallShoppingCartItemVO>>> cartItemPageList(Integer pageNumber, @TokenToMallUser MallUser loginMallUser) {
+        Map params = new HashMap(4);
+        if (pageNumber == null || pageNumber < 1) {
+            pageNumber = 1;
+        }
+        params.put("userId", loginMallUser.getUserId());
+        params.put("page", pageNumber);
+        params.put("limit", Constants.SHOPPING_CART_PAGE_LIMIT);
+        //封装分页请求参数
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(MallShoppingCartService.getMyShoppingCartItems(pageUtil));
+    }
+
+    @GetMapping("/shop-cart")
+    @ApiOperation(value = "购物车列表(网页移动端不分页)", notes = "")
+    public Result<List<MallShoppingCartItemVO>> cartItemList(@TokenToMallUser MallUser loginMallUser) {
+        return ResultGenerator.genSuccessResult(MallShoppingCartService.getMyShoppingCartItems(loginMallUser.getUserId()));
+    }
+
+    @PostMapping("/shop-cart")
+    @ApiOperation(value = "添加商品到购物车接口", notes = "传参为商品id、数量")
+    public Result saveMallShoppingCartItem(@RequestBody SaveCartItemParam saveCartItemParam,
+                                                 @TokenToMallUser MallUser loginMallUser) {
+        String saveResult = MallShoppingCartService.saveMallCartItem(saveCartItemParam, loginMallUser.getUserId());
+        //添加成功
+        if (ServiceResultEnum.SUCCESS.getResult().equals(saveResult)) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //添加失败
+        return ResultGenerator.genFailResult(saveResult);
+    }
+
+    @PutMapping("/shop-cart")
+    @ApiOperation(value = "修改购物项数据", notes = "传参为购物项id、数量")
+    public Result updateNewBeeMallShoppingCartItem(@RequestBody UpdateCartItemParam updateCartItemParam,
+                                                   @TokenToMallUser MallUser loginMallUser) {
+        String updateResult = MallShoppingCartService.updateMallCartItem(updateCartItemParam, loginMallUser.getUserId());
+        //修改成功
+        if (ServiceResultEnum.SUCCESS.getResult().equals(updateResult)) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //修改失败
+        return ResultGenerator.genFailResult(updateResult);
+    }
+
+    @DeleteMapping("/shop-cart/{MallShoppingCartItemId}")
+    @ApiOperation(value = "删除购物项", notes = "传参为购物项id")
+    public Result updateMallShoppingCartItem(@PathVariable("MallShoppingCartItemId") Long MallShoppingCartItemId,
+                                                   @TokenToMallUser MallUser loginMallUser) {
+        MallShoppingCartItem MallCartItemById = MallShoppingCartService.getMallCartItemById(MallShoppingCartItemId);
+        if (!loginMallUser.getUserId().equals(MallCartItemById.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDEN_ERROR.getResult());
+        }
+        Boolean deleteResult = MallShoppingCartService.deleteById(MallShoppingCartItemId);
+        //删除成功
+        if (deleteResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //删除失败
+        return ResultGenerator.genFailResult(ServiceResultEnum.OPERATE_ERROR.getResult());
+    }
+
+    @GetMapping("/shop-cart/settle")
+    @ApiOperation(value = "根据购物项id数组查询购物项明细", notes = "确认订单页面使用")
+    public Result<List<MallShoppingCartItemVO>> toSettle(Long[] cartItemIds, @TokenToMallUser MallUser loginMallUser) {
+        if (cartItemIds.length < 1) {
+            MallException.fail("参数异常");
+        }
+        int priceTotal = 0;
+        List<MallShoppingCartItemVO> itemsForSettle = MallShoppingCartService.getCartItemsForSettle(Arrays.asList(cartItemIds), loginMallUser.getUserId());
+        if (CollectionUtils.isEmpty(itemsForSettle)) {
+            //无数据则抛出异常
+            MallException.fail("参数异常");
+        } else {
+            //总价
+            for (MallShoppingCartItemVO MallShoppingCartItemVO : itemsForSettle) {
+                priceTotal += MallShoppingCartItemVO.getGoodsCount() * MallShoppingCartItemVO.getSellingPrice();
+            }
+            if (priceTotal < 1) {
+                MallException.fail("价格异常");
+            }
+        }
+        return ResultGenerator.genSuccessResult(itemsForSettle);
+    }
+
+}
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Kingsman
+ * @create 2020/6/8
+ * @since 1.0.0
+ */
---
+++ src/main/java/com/mall/mall/api/MallUserAddressAPI.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (C), 2020-2020
+ * FileName: MallUserAddressAPI
+ * Author:   Kingsman
+ * Date:     2020/6/8 0:58
+ * Description:
+ */
+
+package com.mall.mall.api;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Api(value = "v1", tags = "6.新蜂商城个人地址相关接口")
+@RequestMapping("/api/v1")
+
+public class MallUserAddressAPI {
+    @Resource
+    private MallUserAddressService mallUserAddressService;
+
+    @GetMapping("/address")
+    @ApiOperation(value = "我的收货地址列表", notes = "")
+    public Result<List<MallUserAddressVO>> addressList(@TokenToMallUser MallUser loginMallUser) {
+        return ResultGenerator.genSuccessResult(mallUserAddressService.getMyAddresses(loginMallUser.getUserId()));
+    }
+
+    @PostMapping("/address")
+    @ApiOperation(value = "添加地址", notes = "")
+    public Result<Boolean> saveUserAddress(@RequestBody SaveMallUserAddressParam saveMallUserAddressParam,
+                                           @TokenToMallUser MallUser loginMallUser) {
+        MallUserAddress userAddress = new MallUserAddress();
+        BeanUtil.copyProperties(saveMallUserAddressParam, userAddress);
+        userAddress.setUserId(loginMallUser.getUserId());
+        Boolean saveResult = mallUserAddressService.saveUserAddress(userAddress);
+        //添加成功
+        if (saveResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //添加失败
+        return ResultGenerator.genFailResult("添加失败");
+    }
+
+    @PutMapping("/address")
+    @ApiOperation(value = "修改地址", notes = "")
+    public Result<Boolean> updateMallUserAddress(@RequestBody UpdateMallUserAddressParam updateMallUserAddressParam,
+                                                 @TokenToMallUser MallUser loginMallUser) {
+        MallUserAddress mallUserAddressById = mallUserAddressService.getMallUserAddressById(updateMallUserAddressParam.getAddressId());
+        if (!loginMallUser.getUserId().equals(mallUserAddressById.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDEN_ERROR.getResult());
+        }
+        MallUserAddress userAddress = new MallUserAddress();
+        BeanUtil.copyProperties(updateMallUserAddressParam, userAddress);
+        userAddress.setUserId(loginMallUser.getUserId());
+        Boolean updateResult = mallUserAddressService.updateMallUserAddress(userAddress);
+        //修改成功
+        if (updateResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //修改失败
+        return ResultGenerator.genFailResult("修改失败");
+    }
+
+    @GetMapping("/address/{addressId}")
+    @ApiOperation(value = "获取收货地址详情", notes = "传参为地址id")
+    public Result<MallUserAddressVO> getMallUserAddress(@PathVariable("addressId") Long addressId,
+                                                              @TokenToMallUser MallUser loginMallUser) {
+        MallUserAddress mallUserAddressById = mallUserAddressService.getMallUserAddressById(addressId);
+       MallUserAddressVO MallUserAddressVO = new MallUserAddressVO();
+        BeanUtil.copyProperties(mallUserAddressById, MallUserAddressVO);
+        if (!loginMallUser.getUserId().equals(mallUserAddressById.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDEN_ERROR.getResult());
+        }
+        return ResultGenerator.genSuccessResult(MallUserAddressVO);
+    }
+
+    @GetMapping("/address/default")
+    @ApiOperation(value = "获取默认收货地址", notes = "无传参")
+    public Result getDefaultMallUserAddress(@TokenToMallUser MallUser loginMallUser) {
+        MallUserAddress mallUserAddressById = mallUserAddressService.getMyDefaultAddressByUserId(loginMallUser.getUserId());
+        return ResultGenerator.genSuccessResult(mallUserAddressById);
+    }
+
+    @DeleteMapping("/address/{addressId}")
+    @ApiOperation(value = "删除收货地址", notes = "传参为地址id")
+    public Result deleteAddress(@PathVariable("addressId") Long addressId,
+                                @TokenToMallUser MallUser loginMallUser) {
+        MallUserAddress mallUserAddressById = mallUserAddressService.getMallUserAddressById(addressId);
+        if (!loginMallUser.getUserId().equals(mallUserAddressById.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDEN_ERROR.getResult());
+        }
+        Boolean deleteResult = mallUserAddressService.deleteById(addressId);
+        //删除成功
+        if (deleteResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //删除失败
+        return ResultGenerator.genFailResult(ServiceResultEnum.OPERATE_ERROR.getResult());
+    }
+
+}
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Kingsman
+ * @create 2020/6/8
+ * @since 1.0.0
+ */
